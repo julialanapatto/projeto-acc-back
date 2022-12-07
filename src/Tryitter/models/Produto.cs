67b544b02f8d6de565f85b18c5da2c00e4118df5
Klecianny Melo <kleciannymelo@gmail.com>
2022-12-07 13:30:42 -0300

    :sparkles: relacionamento 1:n entre categoria e pr

--- src/Tryitter/models/Produto.cs
+++ src/Tryitter/models/Produto.cs
@@ -9,5 +9,7 @@ namespace Tryitter.Models
     public string? ImagemUrl { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
   }
 }
